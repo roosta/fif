3865f12b4a786a6a7e875144ce21dda1e6358e21
Daniel Berg <mail@roosta.sh>
2021-04-07 12:52:11 +0200

    Bump version to v0.2

--- fif.plugin.zsh
+++ fif.plugin.zsh
@@ -18,7 +18,7 @@
 # ====================================
 
 FIF_CURRENT_DIR=$(cd "$(dirname "$(realpath "${BASH_SOURCE:-$0}")")" && pwd)
-_FIF_VERSION="0.1"
+_FIF_VERSION="0.2"
 
 export FIF_EDITOR_SCRIPT="${FIF_EDITOR_SCRIPT:-$FIF_CURRENT_DIR/editor.sh}"
 
